f0a7d452953e191a732ae923b81fc38f98ca39a6
ZiperMK <stefan.mzt.stojkovski@gmail.com>
2023-02-02 13:56:53 +0100

    update

--- Main/home.php
+++ Main/home.php
@@ -5,5 +5,7 @@ session_start();
 <body>
     <h1>HomePage</h1>
     <a href="../Main/addpost.php"> Post</a>
+    <a href="../Main/favorites.php"> Favorites</a>
+    <a href="../Main/posts.php"> Posts</a>
 </body>
 </html>
